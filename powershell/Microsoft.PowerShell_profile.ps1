87b130624119d047647bc68125fdcc7caa2cbb64
Bernardo Lopes <bernardo.rzr@gmail.com>
2021-12-26 16:17:10 +0000

    Update Powershell profile, Git config and Windows Terminal settings (December 2021)

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,22 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-Prompt -Theme Powerlevel10k_Lean
+Set-PoshPrompt -Theme star
+Import-Module -Name Terminal-Icons
+
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+
+function grep {
+    Param ([string]$text,[string]$types,[string]$relativePath)
+
+    $currentPath = Get-Location
+    $resolvedPath = Join-Path $currentPath $relativePath
+
+    if ($null -eq $types) {
+        $types = "*.*"
+    }
+
+    astrogrep /spath="$resolvedPath" /stypes="$types" /stext="$text" /s
+}
